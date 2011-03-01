67458d646f720634c4cb23fb240f100dad038378
Devin <devin@devin-laptop.(none)>
2011-03-01 11:19:42 -0700

    Added incomplete assembler to the files. Working on getting a register class implemented.

---
+++ assembler.h
@@ -0,0 +1,18 @@
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
+#include <QFile>
+#include <QTextStream>
+#include <qstring.h>
+
+static void process_line(const QByteArray &)
+{
+}
+
+static void process_line(const QString &)
+{
+}
+
+
+
+#endif // ASSEMBLER_H
+
