a72bdb3b8e9f90f0c0f099eaae65668504f0d865
Devin <devin@devin-laptop.(none)>
2011-04-04 21:13:44 -0600

    Made changes and added more function op codes

--- assembler.cpp
+++ assembler.cpp
@@ -11,6 +11,7 @@ const unsigned int LW_OP  = 0x23;
 const unsigned int SW_OP  = 0x2B;
 const unsigned int BEQ_OP = 0x04;
 const unsigned int JMP_OP = 0x02;
+const unsigned int TARGET = 0;
 
 const unsigned int OP_SHIFT = 26;
 const unsigned int FUNC_SHIFT = 0;
@@ -21,8 +22,7 @@ const unsigned int OP_MASK = 0x3F << OP_SHIFT;
 
 const unsigned int ADD_FUNC = 0x20;
 const unsigned int SUB_FUNC = 0x22;
-const unsigned int RSshift_ADDSUB = 21;
-const unsigned int RTshift_ADDSUB = 16;
+
 
 const unsigned int NUM_REGISTERS = 32;
 
@@ -52,6 +52,10 @@ QByteArray parseAssembly(QTextStream assembly)
     else if (words[0] == "ORI")
     {
       instruction |= ORI_OP << OP_SHIFT;
+    }
+    else if (words[0] == "JUMP")
+    {
+        instruction |= JMP_OP << OP_SHIFT;
     }
     else
     {
@@ -97,6 +101,7 @@ QByteArray parseAssembly(QTextStream assembly)
           // parse the register numbers
           QRegExp rx("^\\s.\\$(\\d+)\\s.,\\s.\\$(\\d+)\\s.,\\s.\\$(\\d+)\\s.$");
           int pos = rx.indexIn(words[1]);
+
           if(pos < 0)
           {
             Q_ASSERT(false);  // TODO: throw an exception
@@ -107,16 +112,27 @@ QByteArray parseAssembly(QTextStream assembly)
             Q_ASSERT(false);  // TODO: throw an exception
           }
 
-          unsigned int rs = QVariant(registers[1]).toInt();
+          unsigned int rs = QVariant(registers[0]).toInt();
           if(rs >= NUM_REGISTERS)
             Q_ASSERT(false);  // TODO: throw an exception
-          unsigned int rt = QVariant(registers[2]).toInt();
+          unsigned int rt = QVariant(registers[1]).toInt();
           if(rt >= NUM_REGISTERS)
             Q_ASSERT(false);  // TODO: throw an exception
+          unsigned int IMM16 = QVariant(registers[2]).toInt();
+
+          rs |= IMM16;
 
           instruction |= rs << RS_SHIFT;
           instruction |= rt << RT_SHIFT;
       }
+    case JMP_OP:
+      {
+          // Parse the target value
+
+      }
+
+
+
       default:
         Q_ASSERT(false);  // TODO: throw an exception
     }
